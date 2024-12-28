ac5472d5c2ddd8edf7c7f6144a7d703a211f4934
Samuel Hope <samuelhope805@gmail.com>
2024-12-28 19:10:32 +0300

    second commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,29 +1,46 @@
 import React from 'react';
 import Link from 'next/link';
+import { FaInfoCircle, FaCogs, FaEnvelope } from 'react-icons/fa'; // Import icons
 
 const Header: React.FC = () => {
   return (
     <header className="bg-black text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
         {/* Logo */}
-        <div className="text-2xl font-semibold">
+        <div className="text-xl sm:text-2xl font-semibold"> {/* Adjust logo size for small screens */}
           <Link href="/">
             <span className="text-white">Imani</span> <span className="text-yellow-500">Imports</span>
           </Link>
         </div>
 
-
-
         {/* Navigation Links */}
         <nav>
           <ul className="flex space-x-4">
-            <li>
+            {/* Use icons instead of text on small screens */}
+            <li className="sm:hidden">
+              <Link href="/about">
+                <FaInfoCircle className="text-white" />
+              </Link>
+            </li>
+            <li className="sm:hidden">
+              <Link href="/services">
+                <FaCogs className="text-white" />
+              </Link>
+            </li>
+            <li className="sm:hidden">
+              <Link href="/contact">
+                <FaEnvelope className="text-white" />
+              </Link>
+            </li>
+
+            {/* Text links for larger screens */}
+            <li className="hidden sm:block">
               <Link href="/about">About</Link>
             </li>
-            <li>
+            <li className="hidden sm:block">
               <Link href="/services">Services</Link>
             </li>
-            <li>
+            <li className="hidden sm:block">
               <Link href="/contact">Contact</Link>
             </li>
           </ul>
@@ -34,3 +51,4 @@ const Header: React.FC = () => {
 };
 
 export default Header;
+
